4e60a350b55a3b5412b70b174dd7b666b66bf35a
cramdesign <mcram@Cram-SSU-iMac.local>
2015-02-11 10:22:25 -0500

    loop through all posts
    check for shortcode for all posts.

--- magnifique.php
+++ magnifique.php
@@ -19,21 +19,25 @@
 -------------------------------------------------------------- */
 if ( !function_exists( 'magnifique_scripts' ) ) : function magnifique_scripts() {
 
-	global $post;
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
 	
-	if ( has_shortcode( $post->post_content, 'gallery' ) ) {
+		if ( has_shortcode( get_the_content(), 'gallery' ) ) :
+			
+	
+			// magnific lightbox
+			wp_enqueue_script( 'magnific', plugins_url( '/js/magnific/magnific.js', __FILE__ ), array('jquery') );
+			wp_enqueue_style( 'magnific', plugins_url( '/js/magnific/magnific.css', __FILE__ ) );
+			
+	
+			// start up lightbox
+			wp_enqueue_script( 'init', plugins_url( '/js/init.js', __FILE__ ), array('jquery') );
+			
+			
+		endif;
 		
-
-		// magnific lightbox
-		wp_enqueue_script( 'magnific', plugins_url( '/js/magnific/magnific.js', __FILE__ ), array('jquery') );
-		wp_enqueue_style( 'magnific', plugins_url( '/js/magnific/magnific.css', __FILE__ ) );
-		
-
-		// start up lightbox
-		wp_enqueue_script( 'init', plugins_url( '/js/init.js', __FILE__ ), array('jquery') );
-		
-	}
-
+	endwhile; endif;
+	rewind_posts();
+	
 } endif;
 
 add_action( 'wp_enqueue_scripts', 'magnifique_scripts' );
